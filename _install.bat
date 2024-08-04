e0910b9a29b9639d9c9d6b226317c8795271d397
derycck <derycck@gmail.com>
2024-08-04 15:27:07 -0300

    add script de instalacao de dependencias

---
+++ _install.bat
@@ -0,0 +1,7 @@
+call .venv\Scripts\activate
+python -m pip install --upgrade pip
+printf "call .venv\scripts\\activate\ncd %p%\npython -m pip install --upgrade pip\ncmd /k\n" > activate_venv.bat
+printf "call .venv\scripts\\activate\ncd %p%\npython -m pip install --upgrade pip\npip install -r requirements.txt --upgrade\ncmd /k\n" > activate_venv_update.bat
+pip install black
+
+cmd /k

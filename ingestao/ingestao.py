81df13aa12a186ec00d59b6a016e9fe7b600aff7
derycck <derycck@gmail.com>
2024-09-01 17:56:49 -0300

    refact: ingestao.py. tabelas de cada mes e material_pivot

--- ingestao/ingestao.py
+++ ingestao/ingestao.py
@@ -50,6 +50,37 @@ def preprocess(dataframe: pd.DataFrame, col_name: str) -> pd.DataFrame:
     ).rename(columns=lambda x: x.upper())
 
 
+def preprocess_(data_folder, file_name, column_to_correct):
+    file_path = Path(data_folder) / file_name
+    table_name = Path(file_name).stem.replace("Sample_", "").upper()
+    df = preprocess(
+        pd.read_csv(file_path, encoding="utf8"),
+        column_to_correct,
+    )
+    return {"df": df, "table_name": table_name}
+
+
+def get_gestao_de_falta_por_mes(df_gestao_faltas):
+
+    return (
+        df_gestao_faltas.assign(ANO=lambda x: x["DAT_LIMITE"].str[:4])
+        .assign(MES=lambda x: x["DAT_LIMITE"].str[5:7])
+        .query('ANO=="2022"')
+    )
+
+
+def get_material_pivot(df_gestao_faltas):
+    return (
+        df_gestao_faltas.reindex(columns=["DESCR_MAT_FALT", "CENTRO_FORN"])
+        .groupby(["DESCR_MAT_FALT", "CENTRO_FORN"])["CENTRO_FORN"]
+        .count()
+        .unstack(level=-1)
+        .reset_index()
+        .rename_axis(None, axis=1)
+        .fillna(0)
+    )
+
+
 def main():
     """
     Ingerindo arquivos CSV no DuckDB.
@@ -57,19 +88,20 @@ def main():
     2. Conecta-se ao banco de dados DuckDB.
     3. Cria o esquema "RAW".
     4. Lista todos os arquivos CSV na pasta de dados especificada.
-    5. Itera sobre cada arquivo CSV, pré-processa-o e insere os dados no DuckDB.
+    5. Ingere no DuckDB, as tabelas FACT_TOP_MATERIAL, GESTAO_FALTAS,
+        12 tabelas GESTAO_FALTAS_2022_MM e MATERIAL_PIVOT.
     """
 
-    # Caminho para a pasta que contém os arquivos CSV
-    data_folder = "data"
-    if not Path("data_folder").absolute().exists():
+    # Caminho para a pasta que contém os arquivos CSV)
+    data_folder = Path(__file__).parent / "data"
+    if not Path(data_folder).absolute().exists():
         raise FileNotFoundError(
             f"A pasta {data_folder} não existe.\n"
             + "Salve as bases de dados nela."
         )
 
     # Nome do arquivo DuckDB
-    duckdb_file = Path(".").absolute().parent / "db" / "dev.duckdb"
+    duckdb_file = Path(__file__).parent.absolute().parent / "db" / "dev.duckdb"
     duckdb_file.parent.mkdir(exist_ok=True)
     if duckdb_file.exists():
         print("Removendo arquivo DuckDB existente...\n")
@@ -99,22 +131,64 @@ def main():
 
     # Iterar sobre cada arquivo CSV e inserir os dados no DuckDB
     print("\nInserindo dados no banco de dados DuckDB...")
-    for index, csv_file in enumerate(csv_files):
-        file_path = Path(data_folder) / csv_file
-        table_name = Path(csv_file).stem.replace("Sample_", "").upper()
 
-        # Ler o arquivo CSV usando pandas
-        df = preprocess(
-            pd.read_csv(file_path, encoding="utf8"), cols_to_correct[index]
+    dict_return = preprocess_(
+        data_folder, csv_files[0], "fTOP_DESCRICAO_ECODE"
+    )
+    df_fact_top_material = dict_return["df"]
+    table_name_fact_top_material = dict_return["table_name"]
+    del dict_return
+    print(
+        f"Inserindo tabela raw.{table_name_fact_top_material} - arquivo "
+        + f"{csv_files[0]}"
+    )
+    con.execute(
+        f"CREATE TABLE IF NOT EXISTS raw.{table_name_fact_top_material} "
+        + "AS SELECT * FROM df_fact_top_material"
+    )
+
+    # preprocess_(data_folder, file_name, col_name):
+    dict_return = preprocess_(data_folder, csv_files[1], "DESCR_MAT_FALT")
+    df_gestao_faltas = dict_return["df"]
+    table_name_gestao_Faltas = dict_return["table_name"]
+    del dict_return
+    print(
+        f"Inserindo tabela raw.{table_name_gestao_Faltas} - arquivo "
+        + f"{csv_files[1]}"
+    )
+    con.execute(
+        f"CREATE TABLE IF NOT EXISTS raw.{table_name_gestao_Faltas} "
+        + "AS SELECT * FROM df_gestao_faltas"
+    )
+
+    # con.close()
+    # exit()
+    # Ingerir tabelas GESTAO_FALTAS_2022_MM
+    df_gestao_faltas_2022_por_mes = get_gestao_de_falta_por_mes(
+        df_gestao_faltas
+    )
+    for mes in range(1, 12 + 1):
+        mes_str = f"{mes:02d}"
+        df_mes = df_gestao_faltas_2022_por_mes.query(
+            f'MES=="{mes_str}"'
+        ).reset_index(drop=True)
+        tb_name = f"GESTAO_FALTAS_2022_{mes_str}"
+        cmd_str = (
+            f"CREATE TABLE IF NOT EXISTS raw.{tb_name} AS SELECT * FROM df_mes"
         )
+        print(f"Inserindo tabela raw.{tb_name}")
+        con.execute(cmd_str)
+    del df_gestao_faltas_2022_por_mes, df_mes, tb_name, cmd_str, mes_str
 
-        con.execute(
-            f"CREATE TABLE IF NOT EXISTS raw.{table_name} AS SELECT * FROM df"
-        )
-        print(f"Arquivo {csv_file} inserido na tabela raw.{table_name}")
-
-    # TODO: Ingerir tabelas GESTAO_FALTAS_2022_MM
-    # TODO: Ingerir tabela df_material_pivot
+    # Ingerir tabela df_material_pivot
+    df_material_pivot = get_material_pivot(df_gestao_faltas)
+    tb_name = "MATERIAL_PIVOT"
+    cmd_str = (
+        f"CREATE TABLE IF NOT EXISTS raw.{tb_name} "
+        + "AS SELECT * FROM df_material_pivot"
+    )
+    print(f"Inserindo tabela raw.{tb_name}")
+    con.execute(cmd_str)
 
     # Fechar a conexão com o banco de dados
     con.close()

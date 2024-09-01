e211c9da21e7cb9220bcff4c0013a0003ac76b6a
derycck <derycck@gmail.com>
2024-09-01 17:48:00 -0300

    chore: _install.bat atualizacao

--- _install.bat
+++ _install.bat
@@ -1,7 +1,10 @@
-call .venv\Scripts\activate
+call python -m venv ..\.venv
+call ..\.venv\Scripts\activate
 python -m pip install --upgrade pip
-printf "call .venv\scripts\\activate\ncd %p%\npython -m pip install --upgrade pip\ncmd /k\n" > activate_venv.bat
-printf "call .venv\scripts\\activate\ncd %p%\npython -m pip install --upgrade pip\npip install -r requirements.txt --upgrade\ncmd /k\n" > activate_venv_update.bat
-pip install black
+pip install dbt-duckdb pandas
+
+set p=dbt_duckdb
+printf "call .venv\scripts\\activate\ncd %p%\npython -m pip install --upgrade pip\ncmd /k\n" > ..\activate_venv.bat
+printf "call .venv\scripts\\activate\ncd %p%\npython -m pip install --upgrade pip\npip install -r requirements.txt --upgrade\ncmd /k\n" > ..\activate_venv_update.bat
 
 cmd /k
